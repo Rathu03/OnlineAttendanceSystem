5a10f58b29add6c836b3cbc3582309bb8e2ba398
Rathu03 <rathuboy777@gmail.com>
2024-02-28 00:01:52 +0530

    4th commit

--- client/src/components/StaffDashboard.js
+++ client/src/components/StaffDashboard.js
@@ -1,8 +1,79 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { CiCirclePlus } from "react-icons/ci";
 
 const StaffDashboard = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    localStorage.removeItem('email');
+    localStorage.removeItem('token');
+    navigate('../')
+  }
+
+  const data = [
+    {
+    subject_code : "IT 5402",
+    subject_name : "Data analytics and cloud computing",
+    credits: 3,
+    sem : 6
+  },
+  {
+    subject_code : "IT 5402",
+    subject_name : "Data analytics and cloud computing",
+    credits: 3,
+    sem : 6
+  },
+  {
+    subject_code : "IT 5402",
+    subject_name : "Data analytics and cloud computing",
+    credits: 3,
+    sem : 6
+  },
+  {
+    subject_code : "IT 5402",
+    subject_name : "Data analytics and cloud computing",
+    credits: 3,
+    sem : 6
+  },
+  {
+    subject_code : "IT 5402",
+    subject_name : "Data analytics and cloud computing",
+    credits: 3,
+    sem : 6
+  } 
+]
+
   return (
-    <div>StaffDashboard</div>
+    <div className='main-body'>
+      <div className='nav-container'>
+        <h1>Staff Dashboard</h1>
+        <ul>
+          <li onClick={handleClick}>Logout</li>
+        </ul>
+      </div>
+      <div className='staff-dashboard'>
+        <div className='staff-header'>
+          <h1>Attendance List</h1>
+          <CiCirclePlus className='plus-icon' />
+        </div>
+        <div style={{ borderTop: "1px solid white" }}></div>
+        {data.map((item,index) => (
+          <div className='list-room' key={index}>
+          <div className='rooms'>
+            <div className='room-header'>
+              <div>{item.subject_code}</div>
+              <div>{item.subject_name}</div>
+            </div>
+            <div className='room-header'>
+              <div>Sem: {item.sem}</div>
+              <div>Credits: {item.credits}</div>
+            </div>
+          </div> 
+        </div>
+        ))}
+        
+      </div>
+    </div>
   )
 }
 
--- client/src/components/StudentDashboard.js
+++ client/src/components/StudentDashboard.js
@@ -1,8 +1,78 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { CiCirclePlus } from "react-icons/ci";
 
 const StudentDashboard = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    localStorage.removeItem('rollnumber');
+    localStorage.removeItem('token');
+    navigate('../')
+  }
+
+  const data = [
+    {
+    subject_code : "IT 5402",
+    subject_name : "Data analytics and cloud computing",
+    credits: 3,
+    teacher_name: "Swami"
+  },
+  {
+    subject_code : "IT 5402",
+    subject_name : "Data analytics and cloud computing",
+    credits: 3,
+    teacher_name: "Swami"
+  },
+  {
+    subject_code : "IT 5402",
+    subject_name : "Data analytics and cloud computing",
+    credits: 3,
+    teacher_name: "Swami"
+  },
+  {
+    subject_code : "IT 5402",
+    subject_name : "Data analytics and cloud computing",
+    credits: 3,
+    teacher_name: "Swami"
+  },
+  {
+    subject_code : "IT 5402",
+    subject_name : "Data analytics and cloud computing",
+    credits: 3,
+    teacher_name: "Swami"
+  } 
+]
+
   return (
-    <div>StudentDashboard</div>
+    <div className='main-body'>
+      <div className='nav-container'>
+        <h1>Student Dashboard</h1>
+        <ul>
+          <li onClick={handleClick}>Logout</li>
+        </ul>
+      </div>
+      <div className='staff-dashboard'>
+        <div className='staff-header'>
+          <h1>Attendance List</h1>
+        </div>
+        <div style={{ borderTop: "1px solid white" }}></div>
+        {data.map((item,index) => (
+          <div className='list-room' key={index}>
+          <div className='rooms'>
+            <div className='room-header'>
+              <div>{item.subject_code}</div>
+              <div>{item.subject_name}</div>
+            </div>
+            <div className='room-header'>
+              <div>Staff: {item.teacher_name}</div>
+              <div>Credits: {item.credits}</div>
+            </div>
+          </div> 
+        </div>
+        ))}
+        
+      </div>
+    </div>
   )
 }
 
